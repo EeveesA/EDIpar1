6d55febd6b43e156d0ffb2ec98cc305b31b331a8
EeveesA <98490195+EeveesA@users.noreply.github.com>
2022-09-30 22:09:40 -0500

    Add files via upload

---
+++ T2a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int hora;
+    int minutos=0;
+    printf("Introduce las horas\n");
+    scanf("%d", &hora);
+    printf("Introduce los minutos\n");
+    scanf("%d", &minutos);
+    if (hora<=11 && hora >=0)
+        printf("Buenos dias");
+    else if (hora<=17 && hora >=12) printf("Buenas Tardes");
+    else if (hora<=23 && hora >=18) printf("Buenas Noches");
+    else printf("hey fuck you buddy");
+
+
+}
